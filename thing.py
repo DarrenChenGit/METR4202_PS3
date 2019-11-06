ca92858566c934e607f3e005760fe81db87dafde
alcino723 <57127422+alcino723@users.noreply.github.com>
2019-11-06 17:29:57 +1000

    Update thing.py

--- thing.py
+++ thing.py
@@ -7,10 +7,27 @@ from micromelon import *
 rc = RoverController()
 rc.connectIP()
 
-def getimage():
-    #image = Robot.getImageCapture(IMRES.R640x480)
-    image = Robot.getImageCapture(IMRES.R1920x1088)
-    #image = Robot.getImageCapture(IMRES.R1280x720)
+# Global variable
+
+# Select image resolution variable
+res = (1920,1080)
+#res = (1280,720)
+#res = (620,480)
+
+
+delta = 200
+mid = int(res[0]/2)
+R = (0, 0, 255)
+G = (0, 255, 0) 
+B = (255, 0, 0)
+
+def getimage(res):
+    if res[0] == 1920:
+        image = Robot.getImageCapture(IMRES.R1920x1088)
+    if res[0] == 1280:
+        image = Robot.getImageCapture(IMRES.R1280x720)
+    if res[0] == 640:    
+        image = Robot.getImageCapture(IMRES.R640x480)
     image = image.astype(numpy.uint8)
     return image
 
@@ -31,20 +48,31 @@ def adbinarize(image):
     return binary
 
 def qrcodefunction(image):
+    
+    # Scan normal image for qr code
     barcodes = pyzbar.decode(image)
+
+    # Initialize variable
     x = 0
     y = 0
     w = 0
     h = 0
+
+    # camera calibration
     knownwidth = 7 #cm
     knowndistance = 30 #cm
     knowpixelwidth = 425
     focal = (knowpixelwidth*knowndistance)/knownwidth
+
+    # binary image
     gray = grayscale(image)
     bi = adbinarize(gray)
-    
+
+    # Assign pixel location if found qr code
     for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
+        (x, y, w, h) = barcode.rect # x,y = starting point
+        # w = pixel width of qr code
+        # h = pixel height of qr code
 
     # Check if camera found qr code in normal image
     
@@ -59,25 +87,27 @@ def qrcodefunction(image):
             print('No qr code')
 
         else: # Found qr code in binary image
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.rectangle(image, (x, y), (x + w, y + h), G, 2)
             print('found qr code')
            
-    else:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    else: # Found qr code in normal image
+        cv2.rectangle(image, (x, y), (x + w, y + h), G, 2)
         print('found qr code')
         
     
-    dif = x + w/2 - 960
+    dif = x + w/2 - mid
     
-        
-    if x + (w/2) > 1160 or x + (w/2) < 760:
-        if x == 0:
+    # Check if the qr code is in middle area
+    
+    if x + (w/2) > mid + delta or x + (w/2) < mid - delta: 
+        if x == 0: # No qr code found
             print('')
-        else:
-            cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, (0, 0,255), -1)
+            print('')
+        else: # qr code found outside the middle area
+            cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, R, -1)
             print('outside the area')
-    else:
-        cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, (0, 255,0), -1)
+    else: # qr code found inside the middle area
+        cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, G, -1)
         print('Inside the area')
 
     if h == 0:
@@ -94,103 +124,28 @@ def qrcodefunction(image):
         print('')
         print('')
         print('')
-    
-def findbarcode(image):
-    # Find the barcodes in the image
-    barcodes = pyzbar.decode(image)
-    
-    for barcode in barcodes:
-        (x, y, w, h)= barcode.rect
-        cv2.rectangle(image, (x, y), (x + w, y + h)
-            , (0, 255, 0), 2)
         
-        dif = x + w/2 - 960
-        print('mid point of qr code =', x+(w/2))
-        print('horizontal distance from center = ',dif)
-        print('x =', x)
-        print('y =', y)
-        print('w =', w)
-        print('h =', h)
-        
-        
-def checkqr(image):
-    barcodes = pyzbar.decode(image)
-    x = 0
-    y = 0
-    w = 0
-    h = 0
-    for barcode in barcodes:
-        (x, y, w, h)= barcode.rect
-    if x == 0:
-        print('No qr code')
-    else:
-        print('found qr code')
-            
-def findbarcenter(image):
-    barcodes = pyzbar.decode(image)
+def middlearea(image, res, delta):
     
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        if x + (w/2) >1160 or x + (w/2) < 760:
-            cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, (0, 0,255), -1)
-            print('outside the area')
-        else:
-            cv2.circle(image, (x + int(round(w/2)), y + int(round(h/2))), 5, (0, 255,0), -1)
-            print('Inside the area')
+    midd = int(res[0]/2)
+    
+    maxrange = midd + delta
+    minrange = midd - delta
 
-def finddistance(image):
-    
-    knownwidth = 7 #cm
-    knowndistance = 41 #cm
-    knowpixelwidth = 320
-    focal = (knowpixelwidth*knowndistance)/knownwidth
-    
-    barcodes = pyzbar.decode(image)
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        distance = (knownwidth*focal)/w
-        print('distance =', distance)
-        
+    cv2.line(image, (midd,0), (midd,res[1]), G, 1)
+    cv2.line(image, (minrange,0), (minrange,int(res[1])), B, 1)
+    cv2.line(image, (maxrange,0), (maxrange,int(res[1])), B, 1)
+
 def main():
-    
-    image = getimage()
-##    gray = grayscale(image)
-##    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-##    
-##    kernel = numpy.ones((2,2),numpy.uint8)
-##    kernel2 = numpy.ones((5,5),numpy.uint8)
-##    
-##    thg = adbinarize(gray)
-##    
-##    closing = cv2.morphologyEx(thg, cv2.MORPH_CLOSE, kernel)
-##    dilate = cv2.dilate(closing, kernel,iterations = 3)
-##    
-##    
-##    contours, hierarchy = cv2.findContours(dilate,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-    #checkqr(image)
-    #findbarcode(image)
-##    for contour in contours:
-##        if cv2.contourArea(contour)>1000:
-##            cv2.drawContours(image,contour,-1, (0,0,255),2)
-    
-    #findbarcenter(image)
-    #finddistance(image)
+    i = 0
+    image = 0
+    image = getimage(res)
     qrcodefunction(image)
-    
-    delta = 200
-    maxrange = 960 + delta
-    minrange = 960 - delta
-    
-    cv2.line(image, (960,0), (960,1080), (0,0, 255), 1)
-    cv2.line(image, (minrange,0), (minrange,1080), (255,0, 0), 1)
-    cv2.line(image, (maxrange,0), (maxrange,1080), (255,0, 0), 1)
-    
+    middlearea(image, res, delta)
     cv2.imshow('image', image)
+    cv2.waitKey(1000)  
+    
+while True:
     main()
-
-main()    
     
-
-
-    
+        
